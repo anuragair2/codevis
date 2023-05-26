e13acbe0e12b46eb9743598b0adaecc613d503a3
Anurag Singh <unanurag9934@gmail.com>
2023-05-26 18:16:40 +0530

    this is 2nd time i am commiting

--- code1.c
+++ code1.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello, World! \n");
-    printf("this is Anurag singh");
-    return 0;
+    //printf("this is Anurag\n\n");
+   /* printf("this is the basics of the code\n\n");
+    printf("NOW THIS is THE NEW AND THE BASIC CODE\n\n\t");
+    printf("now this is the main of the things\n\n\t");
+   /* printf("\"this is Anurag singh\"");*/
+    //this is comment we are starting thus.
+   // printf("this is Anurag\n");
+    //now we are going to declare a variable
+    /*int num1 = 10;
+    int num2 = 10;
+    int sum = num1 + num2;
+    printf("%d",sum);*/
+    //int mynum1 = 15;
+    //int mynum2;
+    //int num2 = num1;
+    /*mynum2 = mynum1;
+    printf("%d", mynum2);*/
+   // int myage = 18;
+    //char myname = 'a';
+    //char introwords = 'Hii my name is'; //printf("%c",introwords);
+    //printf("my name is: %c");
+    //printf("%d", myage);
+    //return 0;
+  //  #include <stdio.h>
+
+//int main() {
+    //int  x = 5;
+    //int y = 2;
+    //float divi;
+    //divi = (float)x / y;
+    //printf("%2f",divi);
+    const int x = 4; //in this we can set any number fixed such as value of hour and pi
+    int y = 4;//when we write a constant value we must assigned with a value.
+    //x = y;//here it can declare the error
+    printf("%d",x);
+
+
+    //printf("firstno: %f", myname);
+    //printf("secondno: %1f", myage);
+    //printf("my second name is %2f",myname2);
+    //return 0;
+//}
+
+//}
+//this is the main of the things that is going to be the main of the things that is happennin gim the
+// this happening in the world of the things that is going
+//printf("this is Anurag Singh");
+//printf("This is Anurag singh and this is the main of the things that is going to be the the main of the things that");
+//printf("this is the main of the things that is going to be the main of the things that is going to be the main of the things that is going to be the main of the things that is going to be the main of the thing that is going to be the main of the thing ");
+//int x = 8;
+//int y = 4;
+//int sum;
+//sum = x / y;
+//printf("%d",sum)
+int x = 3;
+int y = 4;
+int z;
+z = x;
+printf("%d",z);
+return 0;
+
 }
